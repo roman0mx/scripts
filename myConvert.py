5d470c446199cc1f414275d2c1ba2cf8a0f103a3
Jose Santamaria <roman0mx@gmail.com>
2018-02-07 12:21:36 -0600

    Primera version 201802071221

---
+++ myConvert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import os
+import subprocess
+
+
+## Incluir la '/' al final del Path definido
+myPath="/opt/convert/"
+OrigFileExt = ".sln16"
+DestFileExt = ".g729"
+i=0
+
+myFileList = os.listdir(myPath)
+
+for i in range (0, len(myFileList)):
+#        print myPath
+#        print i
+
+        if os.path.isfile(myFileList[i]):
+#               print myFileList[i]
+                fn, fxt = os.path.splitext(myFileList[i])
+#                print fn
+#                print fxt
+
+                if (fxt == OrigFileExt):
+                        subprocess.call(["asterisk", "-rx", "file convert " + myPath + myFileList[i] + " " + myPath + fn + DestFileExt])
+
+## Fin
